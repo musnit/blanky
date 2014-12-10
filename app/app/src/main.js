01c10e67190dfd13d23581c026c1db4dc94f50d0
musnit <musnit@gmail.com>
2014-12-10 13:45:28 +0200

    Changed UI and notes

--- app/app/src/main.js
+++ app/app/src/main.js
@@ -5,13 +5,14 @@ define(function(require, exports, module) {
     var Engine = require('famous/core/Engine');
     var AppView = require('views/AppView');
     this.initialTime = Date.now();
+    Engine.setOptions({appMode: false});
 
     var page22And23Model = { popups: [
             {name: 'background', timer: this, url: '/content/images/pages/22and23/background.png'},
             {name: 'wave', timer: this, url: '/content/images/pages/22and23/waves.png', translate: true, translateY: 100, translateX: 100, translateYSpeed: 1000, translateXSpeed: 2000},
             {name: 'ship', timer: this, url: '/content/images/pages/22and23/ship.png', rotate: true, rotateAngle: 12, rotateSpeed: 2000},
-            {name: 'main_pirate', timer: this, url: '/content/images/pages/22and23/main_pirate.png', rotate: true, rotateAngle: 20, rotateSpeed: 2000},
-            {name: 'charlie', timer: this, url: '/content/images/pages/22and23/charlie.png', rotate: true, rotateAngle: 10, rotateSpeed: 2000},
+            {name: 'main_pirate', timer: this, url: '/content/images/pages/22and23/main_pirate.png', translate: true, translateY: 0, translateX: -20, translateYSpeed: 1000, translateXSpeed: 2000, rotate: true, rotateAngle: 20, rotateSpeed: 2000},
+            {name: 'charlie', timer: this, url: '/content/images/pages/22and23/charlie.png', translate: true, translateY: 0, translateX: 20, translateYSpeed: 1000, translateXSpeed: 2000, rotate: true, rotateAngle: 10, rotateSpeed: 2000},
             {name: 'front_rope', timer: this, url: '/content/images/pages/22and23/front_rope.png', rotate: true, rotateAngle: 8, rotateSpeed: 2000}
         ]};
 
