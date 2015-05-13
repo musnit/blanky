a1f44340c0052efba3f27d5d7bcb2bae8c59aca7
musnit <musnit@gmail.com>
2015-05-13 18:00:57 +0800

    fixtures

--- app/app/src/BlankyApp.js
+++ app/app/src/BlankyApp.js
@@ -7,7 +7,7 @@ define(function(require, exports, module) {
     this.Sound = require('soundjs');
     this.Engine.setOptions({appMode: false});
 
-    this.initialPageId = 'UHGPYzstxO';
+    this.initialPageId = '0KdArIgF1J';
     window.initialPageId = this.initialPageId;
 
     // create the main context
