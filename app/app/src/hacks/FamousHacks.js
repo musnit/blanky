fd9da900f120b35d071553ec1070e6b16b694dd2
musnit <musnit@gmail.com>
2015-05-20 22:36:18 +0700

    Change hack for iphone

--- app/app/src/hacks/FamousHacks.js
+++ app/app/src/hacks/FamousHacks.js
@@ -8,11 +8,11 @@ define(function(require, exports, module) {
     function _formatCSSTransform(m) {
         var deviceWidth = window.orientation === 0 ? window.screen.width : window.screen.height;
         var deviceHeight = window.orientation === 0 ? window.screen.height : window.screen.width;
-        // iOS returns available pixels, Android returns pixels / pixel ratio
+        // iOS returns logical pixels, Android returns physical pixels
         // http://www.quirksmode.org/blog/archives/2012/07/more_about_devi.html
-        if (navigator.userAgent.indexOf('Android') >= 0 && window.devicePixelRatio) {
-          deviceWidth = deviceWidth / window.devicePixelRatio;
-          deviceHeight = deviceHeight / window.devicePixelRatio;
+        if (navigator.userAgent.indexOf('iPhone') >= 0 && window.devicePixelRatio) {
+          deviceWidth = deviceWidth * window.devicePixelRatio;
+          deviceHeight = deviceHeight * window.devicePixelRatio;
         }
         m[12] = Math.round(m[12] * deviceWidth) / deviceWidth;
         m[13] = Math.round(m[13] * deviceHeight) / deviceHeight;
