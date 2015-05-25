7905ec43496af2e0e13f473dd3cf9d2aad420be0
musnit <musnit@gmail.com>
2015-05-25 18:38:24 +0700

    Move onclick into own event function

--- app/app/src/app.js
+++ app/app/src/app.js
@@ -9,10 +9,21 @@ define(function(require, exports, module) {
     var loopingIDs = ['UHGPYzstxO','gBqF9PtfBm','w9zCNnEbfC','Du6zsqF3x0'];
     var loopNum = 1;
     var BlankyApp = require('BlankyApp');
+    var clicked = function(event) {
+        var x = event.clientX;
+        if (x < document.getElementById('device-screen').getBoundingClientRect().width/2){
+            window.orientationController.reset();
+        }
+        else {
+            window.blanky.clearPage();
+            window.blanky.loadPage(loopingIDs[loopNum%loopingIDs.length]);
+            loopNum++;
+        }
+    };
     var startApp = function(isApp) {
         window.blanky = new BlankyApp(isApp);
-
         window.blanky.loadPage(window.initialPageId);
+        window.addEventListener('click', clicked, false);
     };
 
     var isApp = document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1;
@@ -23,15 +34,4 @@ define(function(require, exports, module) {
         startApp(false);
     }
 
-    document.onclick= function(event) {
-        var x = event.clientX;
-        if (x < window.screen.width/2){
-            window.orientationController.reset();
-        }
-        else {
-            window.blanky.clearPage();
-            window.blanky.loadPage(loopingIDs[loopNum%loopingIDs.length]);
-            loopNum++;
-        }
-    };
 });
